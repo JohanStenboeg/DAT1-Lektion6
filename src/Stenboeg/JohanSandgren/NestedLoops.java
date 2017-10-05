f0ec0ede985b8f181b1e399222e8a5fb115fb3de
Johan <Joha6163@edu.easj.dk>
2017-10-05 13:44:04 +0200

    Tilføjet en masse ting i forskellige classes

---
+++ src/Stenboeg/JohanSandgren/NestedLoops.java
@@ -0,0 +1,17 @@
+package Stenboeg.JohanSandgren;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        int i, j, resultat;
+        for (j = 1; j <= 10; j++) {
+            for (i = 1; i <= 10; i++) {
+                resultat = i * j;
+                if (resultat % 10 == 0) {
+                    System.out.print("*");
+                    System.out.print(resultat + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
